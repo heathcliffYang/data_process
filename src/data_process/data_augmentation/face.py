bb6a7388a296bab8cbc086a4a1a6b664f4bd3e4c
heathcliffYang <ginny0922fc2@gmail.com>
2021-06-09 15:17:56 +0800

    add augment_hsv

--- src/data_process/data_augmentation/face.py
+++ src/data_process/data_augmentation/face.py
@@ -316,6 +316,20 @@ class FaceDA(object):
         #cv2.imwrite('gray_test_face.jpg', img)
         return img
 
+    # Code ref: https://github.com/ultralytics/yolov5/blob/master/utils/datasets.py
+    def augment_hsv(img, hgain=0.5, sgain=0.5, vgain=0.5):
+        r = np.random.uniform(-1, 1, 3) * [hgain, sgain, vgain] + 1  # random gains
+        hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
+        dtype = img.dtype  # uint8
+
+        x = np.arange(0, 256, dtype=r.dtype)
+        lut_hue = ((x * r[0]) % 180).astype(dtype)
+        lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
+        lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
+
+        img_hsv = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val)))
+        cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
+
 
 
 def fd_get_bboxes(file_path):
